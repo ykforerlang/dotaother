9c24e3cdfeab3de4091ba2b53c53d58aa6d33979
ykforerlang <1527997464@qq.com>
2016-07-14 19:14:58 +0800

    init 2016/07/13

---
+++ tmp/MatchDetail.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yk on 2016/7/14.
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+} from 'react-native';
+
+export default class MatcheDetail extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+
+    }
+}
